212d0929267e6745efc6056a37842d2cfcc8cd4b
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-30 14:37:48 -0600

    third

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -10,17 +10,14 @@ class App extends Component {
       food: ["spaghetti", "ice cream", "sushi"]
     }
   }
+
   render() {
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="App">{foodsToDisplay}</div>
     );
   }
 }
