00d241d84b0e9ce57815ecb0cece7e39e4f00637
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-30 16:16:59 -0600

    Finished

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Image from "./Image.js";
+import Image from "./Image";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image myImage={"https://http.cat/409"} />>
+        <Image myImage={"https://http.cat/409"} />
       </div>
     );
   }
